eb3e71c51ae86b2c1d1caf9beccb52e6ad9d428f
Lmickk2 <lukemickan21@icloud.com>
2022-12-21 13:12:39 -0500

    added more prompts

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ const sql = mysql.createConnection({
     database: "employees_db"
   })
 
+
+function questions() {
+
 inquirer.prompt
   ({
       name: "select",
@@ -17,6 +20,10 @@ inquirer.prompt
       choices: [
           "View employees",
           "View department",
+          "View positions",
+          "Add role",
+          "Add employee",
+          "Add department",
           "Exit"
       ]
   })
@@ -29,23 +36,119 @@ inquirer.prompt
           case "View department":
               viewDepartment()
               break;
+          case "View positions":
+              viewPositions()
+              break;  
+          case "Add role":
+              addRole()
+              break;
+          case "Add employee":
+              addEmployee()
+              break;
+          case "Add department":
+              addDepartment()
+              break;
           case "Exit":
               exit()
               break;
       }
   })
+}
 
   function viewEmployee() {
-      const employeeList = 'SELECT id, name FROM employee_name'
-
-      const 
-
+      const employeeList = 'SELECT * FROM employee_name'
+      sql.query(query, function(err, res) {
+        if(err) throw err
+        console.log(res)
+    })
   }
 
 
 
   function viewDepartment() {
-    const employeeList = 'SELECT id, first_name FROM employee_name'
-
+    const department = 'SELECT * FROM department'
+    sql.query(query, function(err, res) {
+        if(err) throw err
+        console.log(res)
+    })
     
 }
+
+function viewPositions() {
+    const positions = 'SELECT positions, FROM role'
+    sql.query(query, function(err, res) {
+        if(err) throw err
+        console.log(res)
+    })
+    
+}
+
+function addRole() {
+    inquirer.prompt
+    ([
+        {
+        name: "role",
+        message: "Add title of role",
+        type: "input",
+        },  
+        
+        {
+        name: "department",
+        message: "Add department of role",
+        type: "input",
+        }
+    ])
+}
+
+
+function addEmployee() {
+    inquirer.prompt
+    ([
+        {
+        name: "first",
+        message: "Add employee first name",
+        type: "input",
+        },
+
+        {
+        name: "last",
+        message: "Add employee last name",
+        type: "input",
+        },  
+
+        {
+        name: "id",
+        message: "Add employee's ID",
+        type: "input",
+        },
+    
+        {
+        name: "role",
+        message: "Add employee's role",
+        type: "input",
+        },  
+        
+        {
+        name: "department",
+        message: "Add employee's department",
+        type: "input",
+        }
+    ])
+    
+}
+
+function addDepartment() {
+    inquirer.prompt
+    ([
+        {
+        name: "department",
+        message: "Add department of role",
+        type: "input",
+        }
+    ])
+}
+
+function quit() {
+    sql.end();
+    process.exit();
+  }
